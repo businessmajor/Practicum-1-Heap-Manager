626fafb51cd3b5deb31c56cac4e079bb8db76a12
businessmajor <92547375+businessmajor@users.noreply.github.com>
2022-11-07 17:32:50 -0500

    implement page table and main test functions

--- practicum1.c
+++ practicum1.c
@@ -235,6 +235,205 @@ double external_fragmentation() {
 // MOVE PAGES TO DISK USING FIFO OR LRU STRATEGY
 void move_to_disk() {}
 
+
+
+
+
+// Codes for PAGE_TABLE here:
+typedef struct page_table {
+        int page;
+        int frame;
+} page_table;
+
+// create hashArray with defined size
+page_table *hashArray[HEAP_CAPACITY];
+page_table *dummyItem;
+
+// simple hash function that will place the key
+int hashCode(int page) {
+        return page % HEAP_CAPACITY;
+}
+
+// search for the frame number given page key
+page_table *get_frame(int page) {
+
+        // get the hash code
+        int hashIndex = hashCode(page);
+
+        // move in array that is empty
+        while (hashArray[hashIndex] != NULL) {
+
+                if (hashArray[hashIndex]->page == page)
+                        return hashArray[hashIndex];
+
+                // next page
+                ++hashIndex;
+
+                // return to start of table
+                hashIndex %= HEAP_CAPACITY;
+        }
+
+        return NULL;
+}
+
+// insert page and frame number in page_table
+void insert_page(int page, int frame) {
+
+        // allocate heap memory for each item in page_table
+        page_table *item = (page_table*)malloc(sizeof(page_table));
+
+        // assign page & frame values
+        item->page = page;
+        item->frame = frame;
+
+        // get the hash code
+        int hashIndex = hashCode(page);
+
+        // look for an empty or remove page line
+        while (hashArray[hashIndex] != NULL && hashArray[hashIndex]->page != -1) {
+
+                // next page
+                ++hashIndex;
+
+                // return to start of table
+                hashIndex %= HEAP_CAPACITY;
+        }
+
+        hashArray[hashIndex] = item;
+
+}
+
+// delete page & frame line
+page_table *delete_pf_pair(page_table *item) {
+
+        // get the page value
+        int page = item->page;
+
+        // get hash code
+        int hashIndex = hashCode(page);
+
+        // check in the array until empty
+        while (hashArray[hashIndex] != NULL) {
+
+                if (hashArray[hashIndex]->page == page) {
+                        page_table *temp = hashArray[hashIndex];
+
+                        // assign a dummy item at removed position
+                        hashArray[hashIndex] = dummyItem;
+
+			// free heap memory - cant free because this is page table
+			// free(item);
+                        return temp;
+                }
+
+                // check next cell position
+                ++hashIndex;
+
+                // return to start of table
+                hashIndex %= HEAP_CAPACITY;
+        }
+
+        return NULL;
+}
+
+// show items in page_table
+void show_page_table() {
+
+        int i = 0;
+
+        for (i = 0; i < HEAP_CAPACITY; i++) {
+
+                if (hashArray[i] != NULL)
+                        printf(" (%d,%d)", hashArray[i]->page, hashArray[i]->frame);
+                else
+                        printf(" ~~ ");
+        }
+
+        printf("\n");
+}
+
+// checks if item is found in page table
+void page_found_display(page_table *item) {
+
+	if (item != NULL)
+                printf("Page: %d | Frame: %d\n", item->page, item->frame);
+        else
+                printf("Frame not found!\n");
+}
+
+// code implementation for FIFO page replacement
+int page_fault_cnt;	// page fault count
+int cap = 4; 		// sample storage capacity value = 4
+int p[100];		// page sequence
+int pg_seq_size = 8;    // sample page sequence size = 8
+int pg_in_storage[100];	// pages that are in storage
+
+
+// helper method - initialize the capacity of page storage to value -1
+void initialize_cap() {
+	page_fault_cnt = 0;
+	int i;
+	for (i = 0; i < cap; i++) 
+		p[i] = -1;
+}
+
+// helper method - check for page hits true/false
+bool isPageHit(int data) {
+
+	bool pageHit = false;
+	int i;
+
+	// iterage over the capacity
+	for (i = 0; i < cap; i++) {
+		if (p[i] == data) {
+			pageHit = true;
+			break;
+		}
+	}
+
+	return pageHit;
+}
+
+// helper method - show the pages on storage
+void showPages_cap() {
+
+	int i;
+	for (i = 0; i < cap; i++) {
+		if (p[i] != -1)
+			printf(" %d", p[i]);
+	}
+}
+
+// FIFO page replacement
+void fifo() {
+
+	// initialize capacity/storage
+	initialize_cap();
+	int i, j;
+
+	// traverse the pages
+	for (i = 0; i < pg_seq_size; i++) {
+
+		printf("\nFor %d :", pg_in_storage[i]);
+		
+		if (isPageHit(pg_in_storage[i]) == false) {
+
+			// 	- page hit: 1. if memory cell is empty; 2. page is not in storage
+			for (j = 0; j < cap-1; j++)
+				p[j] = p[j + 1];
+			
+			p[j] = pg_in_storage[i];
+
+			// 		- increment page fault count
+			page_fault_cnt++;
+			showPages_cap();
+		} else {
+			// 	- page miss: page is in storage 
+			printf("page miss!");
+		}
+	}
+}
+
 int main() {
   // allocate memory until our heap is full
   // call pm_malloc
@@ -242,6 +441,64 @@ int main() {
   // print heap list, move to disk, print heap again
   // demonstrate pm_malloc works when we have enough memory in heap and
   // demonstrate we can offload pages to "disk" when out of space
+
+  // Hashmap checks:
+       	page_table *item1;
+	page_table *item2;
+	page_table *item3;
+	page_table *item4;
+	page_table *item5;
+	page_table *item6;
+	page_table *item7;
+
+        insert_page(1, 20);
+        insert_page(42, 71);
+        insert_page(2, 31);
+        insert_page(13, 22);
+        insert_page(37, 5);
+//        insert_page(2, 25);
+        insert_page(29, 34);
+	insert_page(32, 4);
+
+	// show_page_table();
+
+	// check page table functions here:
+	page_found_display(get_frame(37));	// check frame value of page 37
+	page_found_display(get_frame(2));	// check frame value of page 2
+	page_found_display(get_frame(32));	// check for frame value of page 32
+
+	item7 = get_frame(32);
+	delete_pf_pair(item7);			// delete page 32 page-frame pair
+	page_found_display(get_frame(32));	// check for frame value of page 32
+
+
+	// free heap memory when done with page_table
+	item1 = get_frame(1);
+	item2 = get_frame(42);
+	item3 = get_frame(2);
+	item4 = get_frame(13);
+	item5 = get_frame(37);
+	item6 = get_frame(29);
+	delete_pf_pair(item1);	
+	delete_pf_pair(item2);
+	delete_pf_pair(item3);
+	delete_pf_pair(item4);
+	delete_pf_pair(item5);
+//	delete_pf_pair(get_frame(2));
+	delete_pf_pair(item6);
+
+	page_found_display(get_frame(37));	// check for frame value of page 32
+
+	// FIFO checks:
+	// input pages data
+	pg_in_storage[0] = 4; pg_in_storage[1] = 13; pg_in_storage[2] = 25; pg_in_storage[3] = 31;
+	pg_in_storage[4] = 7; pg_in_storage[5] = 25; pg_in_storage[6] = 21; pg_in_storage[7] = 31;
+
+	// FIFO method
+	fifo();
+	printf("\nTotal number of page faults: %d", page_fault_cnt);  
+	printf("\n");
+
   return 0;
 }
 
